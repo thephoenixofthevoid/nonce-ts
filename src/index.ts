7dcfaef2181bcca4914d7b9cd19f832a1d66e12f
Phoenix The Fallen <thephoenixofthevoid@gmail.com>
2019-04-21 02:20:42 +0300

    beautify

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,26 @@
 //
 const errEndReached = $throw("Non cycled counter reached its maximal value");
 
-function noop(){}
+function noop() { }
 
-export function Counter(current,prefix: Buffer[] = [], oncycle: Function = errEndReached) {
-    const cr = Array.isArray(current) ? [ ...current ] : $zero(current);
+export function Counter(current, prefix: Buffer[] = [], oncycle: Function = errEndReached) {
+    const cr = Array.isArray(current) ? [...current] : $zero(current);
     const id = Buffer.concat(prefix)
-    const sz = Object.freeze([ ...prefix.map(x => x.length), cr.length ])
-    const ln = sz.reduce((x, y) => x+y)
+    const sz = Object.freeze([...prefix.map(x => x.length), cr.length])
+    const ln = sz.reduce((x, y) => x + y)
 
     Object.freeze(prefix)
 
     return Object.assign(next, { sub, prefix, sizes: sz, id, byteLength: ln })
-    
+
     function next() {
         if ($inc(cr)) oncycle();
-        return Buffer.concat([ ...prefix, Buffer.from(cr) ])
+        return Buffer.concat([...prefix, Buffer.from(cr)])
     }
 
-    function sub (n: number = 10, _oncycle: Function = noop) {
+    function sub(n: number = 10, _oncycle: Function = noop) {
         if ($inc(cr)) oncycle();
-        return Counter(n, [ ...prefix, Buffer.from(cr) ], _oncycle)
+        return Counter(n, [...prefix, Buffer.from(cr)], _oncycle)
     }
 }
 
@@ -32,18 +32,18 @@ export function Counter(current,prefix: Buffer[] = [], oncycle: Function = errEn
  */
 
 function $inc(array) {
-  let i = array.length;
-  while (--i >= 0) {
-    if (++array[i] < 256) return false;
-    array[i] = 0;
-  }
-  return true;
+    let i = array.length;
+    while (--i >= 0) {
+        if (++array[i] < 256) return false;
+        array[i] = 0;
+    }
+    return true;
 }
 
 function $throw(message) {
-  return () => {
-    throw new Error(message)
-  }
+    return () => {
+        throw new Error(message)
+    }
 }
 
 function $zero(n) {
